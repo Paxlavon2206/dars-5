f979b3e0aa5d98af56406a23b5355cc0df6b390d
Paxlavon2206 <paxlavon2206@gmail.com>
2024-02-16 07:13:48 +0500

    dars-5

--- src/data/home-data.js
+++ src/data/home-data.js
@@ -1,7 +1,14 @@
-
-
-
-
+import hero1 from "/hero1.png"
+import hero2 from "/hero2.png"
+import hero3 from "/hero3.png"
+import service1 from "/service1.svg"
+import category1 from "/category1.png"
+import category2 from "/category2.png"
+import category3 from "/category3.png"
+import category6 from "/category6.png"
+import news1 from "/news1.png"
+import rating1 from "/rating1.svg"
+import audio1 from "/audio1.png"
 
 export const headerData = [
     {
@@ -27,5 +34,135 @@ export const headerData = [
         name:"Биз хақимизда",
         path: "/aboutus",
 
+    },
+]
+
+export const heroCardData = [
+    {
+        Img: hero1,
+        name: "1984",
+    },
+    {
+        Img: hero2,
+        name: "Code 8",
+    },
+    {
+        Img: hero3,
+        name: "Rich dad poor dad",
+    }
+]
+
+export const serviceData = [
+    {
+        Img:service1,
+        title:"Тезкор етказиш",
+        cap: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+    },
+    {
+        Img:service1,
+        title:"Тўлов химояси",
+        cap: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+    },
+    {
+        Img:service1,
+        title:"Юқори сифат",
+        cap: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+    },
+    {
+        Img:service1,
+        title:"Энг сара китоблар",
+        cap: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+    }
+]
+
+export const categoryData =[
+    {
+        img:category1,
+        title:"Жахон адабиёти",
+    },
+    {
+        img:category2,
+        title:"Узбек адабиёти",
+    },
+    {
+        img:category3,
+        title:"Бизнес ва психология",
+    },
+    {
+        img:category1,
+        title:"Болалар адабиёти ",
+    },
+    {
+        img:category1,
+        title:"Детективлар",
+    },
+ {
+        img:category6,
+        title:"Фантастика",
+    }
+]
+
+export const newsData = [
+    {
+        img:news1,
+        title:"1984",
+        info: "SIYOSAT, FANTASTIKA",
+        rating:rating1,
+    },
+    {
+        img:news1,
+        title:"1984",
+        info: "SIYOSAT, FANTASTIKA",
+        rating:rating1,
+    },
+    {
+        img:news1,
+        title:"1984",
+        info: "SIYOSAT, FANTASTIKA",
+        rating:rating1,
+    },
+    {
+        img:news1,
+        title:"1984",
+        info: "SIYOSAT, FANTASTIKA",
+        rating:rating1,
+    },
+    {
+        img:news1,
+        title:"1984",
+        info: "SIYOSAT, FANTASTIKA",
+        rating:rating1,
+    },
+]
+export const audioData = [
+    {
+        img:audio1,
+        title:"1984",
+        info: "SIYOSAT, FANTASTIKA",
+        rating:rating1,
+    },
+    {
+        img:audio1,
+        title:"1984",
+        info: "SIYOSAT, FANTASTIKA",
+        rating:rating1,
+    },
+    {
+        img:audio1,
+        title:"1984",
+        info: "SIYOSAT, FANTASTIKA",
+        rating:rating1,
+    },
+    {
+        img:audio1,
+        title:"1984",
+        info: "SIYOSAT, FANTASTIKA",
+        rating:rating1,
+    },
+    {
+        img:audio1,
+        title:"1984",
+        info: "SIYOSAT, FANTASTIKA",
+        rating:rating1,
     },
 ]
--- src/layout/header/header.jsx
+++ src/layout/header/header.jsx
@@ -6,20 +6,24 @@ import { headerData } from "../../data/home-data";
 
 export const Header = () => {
   return (
-   <header>
-     <div className="container">
-      <div className={style.header__container}>
-        <Search />
-        <HeaderRegistarion />
+    <header>
+      <div className="container">
+        <div className={style.header__container}>
+          <Search />
+          <HeaderRegistarion />
+        </div>
       </div>
-    </div>
-    <div className={style.header__bottom}>
-            {headerData.map((item) => <ul className={style.header__list} key={item.path}>
-                <li>
-                    <a className={style.header_link} href={item.path}>{item.name}</a>
-                </li>
-            </ul>)}
-    </div>
-   </header>
+      <div className={style.header__bottom}>
+        {headerData.map((item) => (
+          <ul className={style.header__list} key={item.path}>
+            <li>
+              <a className={style.header_link} href={item.path}>
+                {item.name}
+              </a>
+            </li>
+          </ul>
+        ))}
+      </div>
+    </header>
   );
 };
